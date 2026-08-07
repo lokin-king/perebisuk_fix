f7419dcecc10d10b43dfcbad33b7673aade804fb
ALEX <87030256+lokin-king@users.noreply.github.com>
2026-08-07 05:41:43 +0300

    Add theme retrieval from localStorage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -78,6 +78,7 @@ export default function Home() {
 
   useEffect(() => {
     const savedTheme = localStorage.getItem("prebisyuk:theme");
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     setDark(savedTheme === "dark");
   }, []);
 
